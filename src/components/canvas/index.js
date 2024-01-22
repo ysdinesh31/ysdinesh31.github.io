4b246ed6f366dc187352c06d3b8bdbee16e4a3a5
Dinesh <ysdinesh31@gmail.com>
2024-01-21 17:21:41 -0700

    changed hero animation

---
+++ src/components/canvas/index.js
@@ -0,0 +1,6 @@
+import EarthCanvas from "./Earth";
+import ComputersCanvas from "./Computers";
+import StarsCanvas from "./Stars";
+import GargantuaCanvas from "./Gargantua";
+
+export { EarthCanvas, ComputersCanvas, StarsCanvas, GargantuaCanvas };
